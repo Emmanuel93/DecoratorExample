5fea55de4d49e17d81ff6e97ee4cee76b9b6cee7
brandon <brandon.acz.1994@hotmail.com>
2018-04-12 13:32:52 -0600

    se agrega slot

---
+++ src/main/java/Slot.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Slot {
+
+    List<Slotable> gameelements = new ArrayList<>();
+
+
+    public void addElementSloteable(Slotable element){
+        this.gameelements.add(element);
+
+    }
+
+
+
+
+}
