f951bc3e1a8c1c0ebbe6a206e8bddc32d51a78c9
brandon <brandon.acz.1994@hotmail.com>
2018-04-13 21:40:49 -0600

    agregando metodos al tablero GameBard

--- src/main/java/GameBoard.java
+++ src/main/java/GameBoard.java
@@ -1,4 +1,6 @@
+import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class GameBoard {
 
@@ -10,6 +12,29 @@ public class GameBoard {
     public GameBoard(List<Slot> slots, Integer length ){
         this.slots = slots;
         this.length = length;
+    }
+
+    public void addSlot(int length){
+
+        for(int i=1; i >=length; i++){
+
+            Slot slot= new Slot(new LinkedList<>());
+            slots.add(slot);
+        }
+
+    }
+
+    public Slot getGameBoardSlot(int n){
+
+        return slots.get(n);
+    }
+
+    public int RollDice(){
+        int min=1;
+        int max=3;
+        int randomNum = ThreadLocalRandom.current().nextInt(min, max + 1);
+
+        return randomNum;
     }
 
 
