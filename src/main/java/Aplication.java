c79894261e8a4e50f270d1afe1956611a5b1514e
brandon <brandon.acz.1994@hotmail.com>
2018-04-14 18:16:42 -0600

    GameBoard ahora verifica si los jugadores salen del tablero

--- src/main/java/Aplication.java
+++ src/main/java/Aplication.java
@@ -1,3 +1,13 @@
+import java.util.Arrays;
+
 public class Aplication {
+
+    public static void main(String[] args){
+
+
+        GameBoard testboard = new GameBoard((20));
+
+        System.out.print(testboard.toString());
+    }
 
 }
